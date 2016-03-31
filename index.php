9d588f5e690a9aec75b0481d0175e5f60b1ace9e
vanbara <barrylove777@hotmail.com>
2016-03-31 11:00:35 +1000

    CSS Record player loading bar.
    CSS Record player loading bar create when I was bored.

---
+++ index.php
@@ -0,0 +1,10 @@
+<div class="DiscHolder" id="CoverEditorLoader" style="width:100px; height:100px; margin:auto; z-index:9999999;">
+    <div class="Stick"></div>
+    <div class="SwingBox">
+        <div class="Disc">
+            <div class="DiscCenter">
+                <p>Loading</p>
+            </div>
+        </div>
+    </div>
+</div>
